246810eb7faa057919b1a4e6114a7fa548e2033b
kyliepost <kylie.postlethwaite00@gmail.com>
2021-09-01 11:18:49 -0500

    wrote function that will render html for a location list in locationList.js and created html page in KandyKorner.js

---
+++ src/locations/locationsList.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react"
+
+export const LocationList = () => {
+    const [locations, setLocations] = useState([])
+
+    useEffect (
+        () => {
+            fetch("http://localhost:8088/locations")
+            .then(res => res.json())
+            .then ((data) => {
+                setLocations(data)
+            })
+        },
+        []
+    )
+
+    return (
+        <>
+            {
+                locations.map(
+                    (locationObject) => {
+                        return <p key={`location--${locationObject.id}`}>{locationObject.city}</p>
+                    }
+                )
+            }
+        </>
+    )
+}
+
