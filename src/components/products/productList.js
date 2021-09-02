ecc22b11ecc19349782c165136d9d8e4da2baa9e
kyliepost <kylie.postlethwaite00@gmail.com>
2021-09-02 10:25:31 -0500

    created a string that tells what type the candy is

--- src/components/products/productList.js
+++ src/components/products/productList.js
@@ -5,7 +5,7 @@ export const ProductList = () => {
 
     useEffect (
         () => {
-            fetch("http://localhost:8088/products")
+            fetch("http://localhost:8088/products?_expand=productType")
             .then(res => res.json())
             .then ((data) => {
                 setProducts(data)
@@ -19,7 +19,7 @@ export const ProductList = () => {
         {
             products.map(
                 (productObject) => {
-                    return <p key={`product--${productObject.id}`}>{productObject.name}</p>
+                    return <p key={`product--${productObject.id}`}>{productObject.name} is a {productObject.productType.type}</p>
                 }
             )
         }
